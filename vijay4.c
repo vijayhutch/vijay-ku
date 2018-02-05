e9ed6f249025f5620bca83e31339eb48b8155a5a
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-05 16:40:28 +0530

    Create vijay4.c

---
+++ vijay4.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    
+    if(a>b && a>c)
+    
+    printf("%d a is a largest number",a);
+    
+    else if(b>c)
+    {
+        printf("%d b is a largest number",b);
+    }
+    else
+    {
+        printf("%d c is a largest number",c);
+    

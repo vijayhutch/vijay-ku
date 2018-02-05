03a01b388384f7d5bf7f23c71e7c3ac24d9b1810
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-05 16:33:45 +0530

    Rename vijay3.c to vijay13.c

---
+++ vijay13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+printf("enter the number"):
+scanf("%d",&a);
+if(a==1)
+{
+printf("alphabets");
+else
+printf(" not");
+getch();
+return 0;
+}

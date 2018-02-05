d9e90d9a811443c2c124a85639b618433aef292d
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-05 16:12:12 +0530

    Create vijay.c

---
+++ vijay.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char a,b,c,d,e;
+printf("enter the alphabets");
+scanf("%c",&a);
+if(a==1)
+{
+printf("enter the vowls :a,b,c,d,e);
+else
+print("the vowels are not");
+return 0;
+}

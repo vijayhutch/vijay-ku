0ce18ab73c5b28dfa68cd877c08259d972639130
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-12 16:03:49 +0530

    Create vijay56.c

---
+++ vijay56.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the a and b:",a,b);
+while(a<b)
+{
+int i,flag=0;
+for(i=2;i<=a/2;i++)
+{
+if(a%i==0)
+{
+break;
+}
+{
+printf("%d",&a);
+a++;
+}
+return 0;
+}

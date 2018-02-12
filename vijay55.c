28a228c241a32c3523aa23c277b38e0b10c1467f
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-12 15:59:52 +0530

    Create vijay55.c

---
+++ vijay55.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+include<conio.h>
+int main()
+{
+ int j,a,b;
+ printf("enter the a value");
+ scanf("%d",&a);
+ printf("enter the b value");
+ scanf("%d",&b);
+ for(i=a;i<=b;i++)
+ {
+  if(i%2==1)
+   printf("%d",j);
+   return 0;
+ }
